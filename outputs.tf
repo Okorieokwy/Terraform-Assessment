d251ba52495db97645949b1b92f34ced7a4b0975
okorieokwy <okorie.okwy@gmail.com>
2026-01-12 18:22:03 +0100

    Remove unused output variables and add outputs for web and database private IPs

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,7 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.main.id
+output "web_1_private_ip" {
+  value = aws_instance.web_1.private_ip
 }
 
-output "bastion_public_ip" {
-  description = "Public IP of the Bastion Host"
-  value       = aws_instance.bastion.public_ip
-}
-
-output "load_balancer_dns_name" {
-  description = "The DNS name of the ALB (Paste this in your browser)"
-  value       = aws_lb.main.dns_name
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+output "db_private_ip" {
+  value = aws_instance.db.private_ip
 }
